3c25376c8dcc3c4e86b5bd20af77e850377b2dfd
adereveanco <adereveanco@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-03 22:32:11 +0000

    Better java docs

--- simulator-core/src/main/java/com/tacitknowledge/simulator/AdapterFactory.java
+++ simulator-core/src/main/java/com/tacitknowledge/simulator/AdapterFactory.java
@@ -7,18 +7,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Factory implementation for the different adapters
+ *
  * @author Jorge Galindo (jgalindo@tacitknowledge.com)
  */
 class AdapterFactory
 {
+    /**
+     * Container for the adapters.
+     */
     private static Map<String, Adapter> adapters = new HashMap<String, Adapter>()
-    {{
+    {
+        {
             put(FormatConstants.JSON, new JsonAdapter());
             put(FormatConstants.XML, new XmlAdapter());
-        }};
+        }
+    };
 
-    public static Adapter getAdapter(String inboundFormat)
+    /**
+     * Returns implementation of the adapter for the provided format.
+     * @param format the format of the data.
+     * @return Adapter for the specified format.
+     */
+    public static Adapter getAdapter(String format)
     {
-        return adapters.get(inboundFormat);
+        return adapters.get(format);
     }
 }
