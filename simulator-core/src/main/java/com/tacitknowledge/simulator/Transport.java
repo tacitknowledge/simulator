a1f4890f2158ea575cba6b3dc16b1b972845e522
adereveanco <adereveanco@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-03 22:18:45 +0000

    Better java docs

--- simulator-core/src/main/java/com/tacitknowledge/simulator/Transport.java
+++ simulator-core/src/main/java/com/tacitknowledge/simulator/Transport.java
@@ -1,10 +1,9 @@
 package com.tacitknowledge.simulator;
 
 /**
- * Wrapper for transport configuration.
- * Each transport implementation should hold the required attributes for each transport type
- * (server, port, queue, path, etc)
- *
+ * Wrapper for transport configuration. Each transport implementation should hold the required
+ * attributes for each transport type (server, port, queue, path, etc)
+ * 
  * @author Jorge Galindo (jgalindo@tacitknowledge.com)
  */
 public interface Transport
@@ -12,15 +11,15 @@ public interface Transport
     /**
      * Returns the Transport type
      *
-     * @return
+     * @return transport type
      */
     String getType();
 
     /**
-     * Returns a valid String URI representation of this transport for Camel route creation
-     * e.g.: file://path/to/file/directory , jms:queue/myqueue ,
+     * Returns a valid String URI representation of this transport for Camel route creation e.g.:
+     * file://path/to/file/directory , jms:queue/myqueue ,
      *
-     * @return
+     * @return URI representation of the transport
      */
     String toUriString();
 }
