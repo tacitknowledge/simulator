eac172efdf3099ba10ac7f4df9d6128ea5b54e5e
adereveanco <adereveanco@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-03 22:35:07 +0000

    Better java docs

--- simulator-core/src/main/java/com/tacitknowledge/simulator/ConversationManager.java
+++ simulator-core/src/main/java/com/tacitknowledge/simulator/ConversationManager.java
@@ -1,39 +1,49 @@
 package com.tacitknowledge.simulator;
 
 /**
+ * Defines the interface for the implementations of the ConversationManager.
+ *
  * @author Jorge Galindo (jgalindo@tacitknowledge.com)
  */
 public interface ConversationManager
 {
     /**
      * Creates a new Conversation from the provided transports and formats
-     *
+     * 
      * @param inboundTransport
      * @param outboundTransport
      * @param inboundFormat
      * @param outboundFormat
      * @return
      */
-    public Conversation createConversation(
-            Transport inboundTransport, Transport outboundTransport, String inboundFormat, String outboundFormat) throws UnsupportedFormatException;
+    public Conversation createConversation(Transport inboundTransport, Transport outboundTransport,
+            String inboundFormat, String outboundFormat) throws UnsupportedFormatException;
 
     /**
      * Creates a new scenario for an existing conversation
-     *
+     * 
      * @param conversationId
-     * @param language       The scripting language for the scenario. This would be System wide.
-     * @param criteria       The criteria script
-     * @param transformation The transformation script
+     * @param language
+     *            The scripting language for the scenario. This would be System wide.
+     * @param criteria
+     *            The criteria script
+     * @param transformation
+     *            The transformation script
      */
-    void createConversationScenario(int conversationId, String language, String criteria, String transformation);
+    void createConversationScenario(int conversationId, String language, String criteria,
+            String transformation);
 
     /**
-     * @param conversationId
+     * Activates the conversation with provided conversation id.
+     *
+     * @param conversationId id of the conversation to be activated.
      */
     void activate(int conversationId);
 
     /**
-     * @param conversationId
+     * Deactivates the conversation with provided conversation id.
+     *
+     * @param conversationId id of the conversation to be deactivated.
      */
     void deactivate(int conversationId);
 }
