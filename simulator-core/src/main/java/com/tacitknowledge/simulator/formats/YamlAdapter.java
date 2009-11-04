afdac5d60b9b57aa3a5e4237acbb6bd25fd7741d
jgalindo <jgalindo@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-04 15:11:50 +0000

    More adapter stubs

---
+++ simulator-core/src/main/java/com/tacitknowledge/simulator/formats/YamlAdapter.java
@@ -0,0 +1,17 @@
+package com.tacitknowledge.simulator.formats;
+
+import com.tacitknowledge.simulator.Adapter;
+import com.tacitknowledge.simulator.SimulatorPojo;
+
+/**
+ * @author galo
+ */
+public class YamlAdapter implements Adapter {
+    public SimulatorPojo adaptFrom(Object o) {
+        return null;
+    }
+
+    public Object adaptTo(SimulatorPojo pojo) {
+        return null;
+    }
+}
