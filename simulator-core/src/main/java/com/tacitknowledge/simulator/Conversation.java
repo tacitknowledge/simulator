cae360e95927d4bf34d71cbad574d75f219faa06
jgalindo <jgalindo@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-04 16:42:14 +0000

    Java doc-cing Conversation

--- simulator-core/src/main/java/com/tacitknowledge/simulator/Conversation.java
+++ simulator-core/src/main/java/com/tacitknowledge/simulator/Conversation.java
@@ -11,23 +11,23 @@ import java.util.List;
 public class Conversation
 {
     /**
-     * 
+     * Conversation ID.
      */
     private Integer id;
     /**
-     *
+     * Wrapper for inbound transport configuration
      */
     private Transport inboundTransport;
     /**
-     *
+     * Wrapper for outbound transport configuration
      */
     private Transport outboundTransport;
     /**
-     *
+     * Wrapper for inbound format adapter
      */
     private Adapter inboundAdapter;
     /**
-     *
+     * Wrapper for outbound format adapter
      */
     private Adapter outboundAdapter;
 
@@ -36,6 +36,14 @@ public class Conversation
      */
     private List<ConversationScenario> scenarios;
 
+    /**
+     *
+     * @param id Conversation ID
+     * @param inboundTransport Wrapper for inbound transport configuration
+     * @param outboundTransport Wrapper for outbound transport configuration
+     * @param inboundAdapter Wrapper for inbound format adapter
+     * @param outboundAdapter Wrapper for outbound format adapter
+     */
     public Conversation(Integer id, Transport inboundTransport, Transport outboundTransport, Adapter inboundAdapter, Adapter outboundAdapter)
     {
         this.id = id;
@@ -45,31 +53,58 @@ public class Conversation
         this.outboundAdapter = outboundAdapter;
     }
 
+    /**
+     * Adds a new Scenario to this Conversation
+     * @param language The scripting language to be used.
+     * @param criteria
+     * @param transformation
+     * @return
+     */
     public ConversationScenario addScenario(String language, String criteria, String transformation)
     {
         return null;
     }
 
+    /**
+     * Returns this conversation inbound transport
+     * @return
+     */
     public Transport getInboundTransport()
     {
         return inboundTransport;
     }
 
+    /**
+     * Returns this conversation outbound transport
+     * @return
+     */
     public Transport getOutboundTransport()
     {
         return outboundTransport;
     }
 
+    /**
+     * Retuns this conversation inbound format adapter
+     * @return
+     */
     public Adapter getInboundAdapter()
     {
         return inboundAdapter;
     }
 
+    /**
+     * Retuns this conversation outbound format adapter
+     * @return
+     */
     public Adapter getOutboundAdapter()
     {
         return outboundAdapter;
     }
 
+    /**
+     * Returns the current list of configured scenarios for this conversation
+     * @return
+     */
     public List<ConversationScenario> getScenarios()
     {
         return scenarios;
