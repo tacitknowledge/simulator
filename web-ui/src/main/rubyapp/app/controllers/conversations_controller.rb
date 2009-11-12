1402642adf5b73de7c3cfa4370788e770e7b86d0
adereveanco <adereveanco@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-12 20:42:12 +0000

    More restful controllers

---
+++ web-ui/src/main/rubyapp/app/controllers/conversations_controller.rb
@@ -0,0 +1,46 @@
+class ConversationsController < ApplicationController
+
+  def index
+    @conversations = Conversation.find_all_by_system_id(params[:system_id])
+
+    render :json => { :data => @conversations }
+  end
+
+  def show
+   @conversation = Conversation.find(params[:id])
+   if(params[:format]==:json)
+     render :json => { :success => true, :data => @conversation }
+   end
+  end
+
+  def create
+    @system = Conversation.new(ActiveSupport::JSON.decode(params[:data]))
+
+    if @system.save
+      render :json => { :success => true, :message => "Created new Conversation #{@conversation.id}", :data => @conversation }
+    else
+      render :json => { :message => "Failed to create Conversation"}
+    end
+  end
+
+  def update
+    @conversation = Conversation.find(params[:id])
+
+    if @conversation.update_attributes(ActiveSupport::JSON.decode(params[:data]))
+      render :json => { :success => true, :message => "Updated Conversation #{@conversation.id}", :data => @conversation }
+    else
+      render :json => { :message => "Failed to update Conversation"}
+    end
+  end
+
+  def destroy
+    @conversation = Conversation.find(params[:id])
+
+    if @conversation.destroy
+      render :json => { :success => true, :message => "Destroyed Conversation #{@conversation.id}" }
+    else
+      render :json => { :message => "Failed to destroy Conversation" }
+    end
+  end
+  
+end
---
+++ web-ui/src/main/rubyapp/app/controllers/scenarios_controller.rb
@@ -0,0 +1,46 @@
+class ScenariosController < ApplicationController
+
+  def index
+    @scenarios = Scenario.find_all_by_conversation_id(params[:conversation_id])
+
+    render :json => { :data => @scenarios }
+  end
+
+  def create
+    @scenario = Scenario.new(ActiveSupport::JSON.decode(params[:data]))
+
+    if @scenario.save
+      render :json => { :success => true, :message => "Created new Scenario #{@scenario.id}", :data => @scenario }
+    else
+      render :json => { :message => "Failed to create Scenario"}
+    end
+  end
+
+  def update
+    @scenario = Scenario.find(params[:id])
+
+    if @scenario.update_attributes(ActiveSupport::JSON.decode(params[:data]))
+      render :json => { :success => true, :message => "Updated Scenario #{@scenario.id}", :data => @scenario }
+    else
+      render :json => { :message => "Failed to update Scenario"}
+    end
+  end
+
+  def show
+   @scenario = Scenario.find(params[:id])
+   if(params[:format]==:json)
+     render :json => { :success => true, :data => @scenario }
+   end
+  end
+
+  def destroy
+    @scenario = Scenario.find(params[:id])
+
+    if @scenario.destroy
+      render :json => { :success => true, :message => "Destroyed Scenario #{@scenario.id}" }
+    else
+      render :json => { :message => "Failed to destroy Scenario" }
+    end
+  end
+  
+end
