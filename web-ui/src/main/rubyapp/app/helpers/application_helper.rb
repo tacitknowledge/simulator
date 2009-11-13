eff2aad85d6e1d126f3b35ed7ab660d151e93b89
adereveanco <adereveanco@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-13 18:46:28 +0000

    Views will now be aware of the context path the app is deployed to

--- web-ui/src/main/rubyapp/app/helpers/application_helper.rb
+++ web-ui/src/main/rubyapp/app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  #Method used in views to build the path to the ext js files
+  #based on the configuration set in the environment variables.
+  def relative_url_root
+    ENV["relative_url_root"]
+  end
 end
