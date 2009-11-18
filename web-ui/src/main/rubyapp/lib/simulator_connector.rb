b6414be8dc08d2c88a8fc5c1cce3c011457bf0de
jgalindo <jgalindo@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-18 21:32:27 +0000

    Contact: First successful communication between the Simulator Java code and the JRuby on Rails GUI app. (how 'bout we include the actual connector?)

---
+++ web-ui/src/main/rubyapp/lib/simulator_connector.rb
@@ -0,0 +1,47 @@
+require 'singleton'
+require 'java'
+require "log4j-1.2.14.jar"
+require "camel-core-2.0.0.jar"
+require "simulator-core-1.0-SNAPSHOT.jar"
+
+include_class 'com.tacitknowledge.simulator.impl.ConversationManagerImpl'
+
+# This class is to be the sole communication way from the JRuby app into the Java Simulator.
+# It should provide methods to access all the functionality provided by ConversationManager.
+# Also, this class should handle all Java-classes that are to be populated and passed to the
+# ConversationManager methods.
+class SimulatorConnector
+  include Singleton
+
+  def initialize
+    @conv_mgr = ConversationManagerImpl.new
+  end
+
+  def get_format_parameters(format)
+    @conv_mgr.getAdapterParameters(format)
+  end
+
+  def get_transport_parameters(transport)
+    # Way to get transport parameters still needs to be implemented
+  end
+
+  def create_conversation
+    #
+  end
+
+  def create_conversation_scenario
+    #
+  end
+
+  def activate
+    #
+  end
+
+  def deactivate
+    #
+  end
+
+  def delete_conversation
+    #
+  end
+end
