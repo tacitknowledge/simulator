911c9011d6bb96a46efda61284a054efed500a66
ogonzalez <ogonzalez@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-11-23 21:40:47 +0000

    Modifying this test a little bit before adding the Ruby support, normally we don't want to catch exceptions in unit tests, if we have an unexpected exception what we want is to break the build inmediatly and fix the test, catching it could hide it or delay the prescence of it, in this version of junit failure != error

--- simulator-core/src/test/java/com/tacitknowledge/simulator/ScriptExecutionServiceTest.java
+++ simulator-core/src/test/java/com/tacitknowledge/simulator/ScriptExecutionServiceTest.java
@@ -1,61 +1,58 @@
 package com.tacitknowledge.simulator;
 
-import junit.framework.TestCase;
 import com.tacitknowledge.simulator.formats.AdapterFactory;
 import com.tacitknowledge.simulator.formats.FormatConstants;
-import com.tacitknowledge.simulator.scripting.ScriptExecutionService;
 import com.tacitknowledge.simulator.scripting.PojoClassGenerator;
+import com.tacitknowledge.simulator.scripting.ScriptExecutionService;
+import javassist.ClassPool;
+import junit.framework.TestCase;
 
 import java.util.Map;
 
-import javassist.ClassPool;
-
 /**
- * @author galo
+ * This test will help us to validate we are able to read and execute the different scripting
+ * languages
+ *
+ * @author JOrge Galindo (jgalindo@tacitknowledge.com)
  */
 public class ScriptExecutionServiceTest extends TestCase
 {
-    public void testEval()
+    /**
+     * This method will test the evaluation for JavaScript
+     * @throws Exception
+     */
+    public void testEvalJavaScript() throws Exception
     {
         // --- Default Javassist' JVM class pool
         ClassPool pool = ClassPool.getDefault();
-        
+
         // --- First, get the SimulatorPojo from the data
         Adapter adapter = AdapterFactory.getAdapter(FormatConstants.XML);
+        // --- Get the data in a strcutured form as a SimulatorPojo
+        SimulatorPojo pojo = adapter.adaptFrom(TestHelper.XML_DATA);
 
-        try
-        {
-            // --- Get the data in a strcutured form as a SimulatorPojo
-            SimulatorPojo pojo = adapter.adaptFrom(TestHelper.XML_DATA);
+        // --- Get a map of (temporary and dinamically generated) beans out of the SimulatorPojo
+        PojoClassGenerator generator = new PojoClassGenerator(pool);
+        Map beans = generator.generateBeansMap(pojo);
+        // --- Now, get a ScriptExecutionService and set the language (javascript)
+        ScriptExecutionService execServ = new ScriptExecutionService();
+        execServ.setLanguage("javascript");
 
-            // --- Get a map of (temporary and dinamically generated) beans out of the SimulatorPojo
-            PojoClassGenerator generator = new PojoClassGenerator(pool);
-            Map beans = generator.generateBeansMap(pojo);
-            // --- Now, get a ScriptExecutionService and set the language (javascript)
-            ScriptExecutionService execServ = new ScriptExecutionService();
-            execServ.setLanguage("javascript");
+        // --- Test a couple of values
+        Object result = execServ.eval("employees", "Get employees", beans);
+        assertNotNull(result);
 
-            // --- Test a couple of values
-            Object result = execServ.eval("employees", "Get employees", beans);
-            assertNotNull(result);
+        result = execServ.eval("employees.reportDate", "Is it today?", beans);
+        assertEquals(result, "2009-11-05");
 
-            result = execServ.eval("employees.reportDate", "Is it today?", beans);
-            assertEquals(result,"2009-11-05");
+        result = execServ.eval("employees.employee[1].name", "Second employee name", beans);
+        assertEquals(result, "Sara");
 
-            result = execServ.eval("employees.employee[1].name", "Second employee name", beans);
-            assertEquals(result,"Sara");
-
-            // --- Detach the generated classes
-            generator.detachGeneratedClasses();
-            // --- Get rid of the reference to the generator and ClassPool
-            generator = null;
-            pool = null;
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            fail();
-        }
+        // --- Detach the generated classes
+        generator.detachGeneratedClasses();
+        // --- Get rid of the reference to the generator and ClassPool
+        generator = null;
+        pool = null;
     }
 
 }
