22b72d0fdb471ff3d4c72fe5958a99ba8ea1baf0
nbelenkiy <nbelenkiy@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-12-15 19:02:01 +0000

    activate all enabled conversations on startup

---
+++ web-ui/src/main/rubyapp/config/initializers/run_active_conversations.rb
@@ -0,0 +1,7 @@
+enabled_conversations = Conversation.find_all_by_enabled(true)
+puts 'Activating all enabled conversations'
+
+enabled_conversations.each do |conversation|
+  puts conversation.name 
+  SimulatorConnector.instance.activate(conversation);
+end
