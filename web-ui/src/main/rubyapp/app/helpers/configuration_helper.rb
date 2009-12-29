a62c8ba7cb60484d839edd411f76205e40cb6066
nbelenkiy <nbelenkiy@1caace6a-b83d-0410-a556-d4fd5157a890>
2009-12-29 21:59:45 +0000

    added import test

--- web-ui/src/main/rubyapp/app/helpers/configuration_helper.rb
+++ web-ui/src/main/rubyapp/app/helpers/configuration_helper.rb
@@ -1,6 +1,7 @@
 module ConfigurationHelper
 
 
+# returns xml representation of systems
   def systems_to_xml(systems)
     doc = REXML::Document.new("<?xml version='1.0?><systems/>")
 
@@ -39,8 +40,29 @@ module ConfigurationHelper
     doc
   end
 
+  def import_xml(doc)
+    doc.root.elements.each do |system_element|
+      name = get_property(system_element, 'name')
+      system = System.find_by_name name
+      if (system.nil?)
+        system = System.new
+        system.name = name;
+        is_new=true
+      else
+        is_new=false
+      end
+      system.description = get_property(system_element, 'description')
+      system.script_language = get_property(system_element, 'script_language')
+      system.save
+
+#      parse conversation
+      parse_conversations(is_new, system, system_element)
+    end
+  end
+
   private
 
+  #!!!!!!!! export
   def create_element parent, tag_name, value
     system_name_element = parent.add_element(tag_name)
     system_name_element.text=value
@@ -62,4 +84,114 @@ module ConfigurationHelper
       create_element configuration_element, configuration.attribute_name, configuration.attribute_value
     end
   end
+
+  #!!!!!!!!!!import
+  def parse_conversations(is_new, system, system_element)
+    conversations_elements = system_element.get_elements("conversation")
+    conversations_elements.each do |conversation_element|
+      conversation_name = get_property(conversation_element, "name")
+      if (is_new)
+        conversation = Conversation.new
+        conversation.name=conversation_name
+        conversation.system_id = system.id
+      else
+        conversation = Conversation.find_by_name_and_system_id(conversation_name, system.id)
+        if (conversation.nil?)
+          conversation = Conversation.new
+          conversation.name = conversation_name
+          conversation.system_id = system.id
+        end
+      end
+      conversation.description=get_property(conversation_element, "description")
+      conversation.default_response=get_property(conversation_element, "default_response")
+
+      in_transport_element = conversation_element.get_elements("inbound_transport")[0]
+      out_transport_element = conversation_element.get_elements("outbound_transport")[0]
+      in_format_element = conversation_element.get_elements("inbound_format")[0]
+      out_format_element = conversation_element.get_elements("outbound_format")[0]
+
+      conversation.in_transport = parse_transport in_transport_element
+      conversation.out_transport = parse_transport out_transport_element
+      conversation.in_format = parse_format in_format_element
+      conversation.out_format = parse_format out_format_element
+      conversation.save
+
+#     parse scenarios for each conversation
+      parse_scenarios(conversation, is_new, conversation_element)
+    end
+  end
+
+  def parse_scenarios(conversation, is_new, conversation_element)
+    scenario_elements = conversation_element.get_elements("scenario")
+    scenarios=[]
+    scenario_elements.each do |scenario_element|
+      scenario_name = get_property(scenario_element, "name")
+      if (is_new)
+        scenario = Scenario.new
+        scenario.name=scenario_name
+        scenario.conversation_id = conversation.id
+      else
+        scenario = Scenario.find_by_conversation_id_and_name(conversation.id, scenario_name)
+        if(scenario.nil?)
+          scenario = Scenario.new
+          scenario.name=scenario_name
+          scenario.conversation_id = conversation.id
+        end
+      end
+      scenario.label=get_property(scenario_element, "label")
+      scenario.criteria_script=get_property(scenario_element, "criteria_script")
+      scenario.execution_script=get_property(scenario_element, "execution_script")
+      scenario.save
+      scenarios<<scenario
+    end
+    scenarios
+  end
+
+
+  def parse_format(format_element)
+    format_type_name = get_property(format_element,"type")
+    format_type = FormatType.find_by_name(format_type_name)
+
+    format = Format.new
+    format.format_type_id = format_type.id
+
+    configuration_element = format_element.get_elements("configuration")[0]
+    configurations=[]
+    configuration_element.elements.each do |config_name|
+      config=FormatConfiguration.new
+      config.attribute_name = config_name.name
+      config.attribute_value = config_name.text
+      configurations << config
+    end
+    format.configurations = configurations
+    format
+  end
+
+  def parse_transport(transport_element)
+    transport_type_name = get_property(transport_element,"type")
+    transport_type = TransportType.find_by_name(transport_type_name)
+
+    transport = Transport.new
+    transport.transport_type_id = transport_type.id
+
+    configuration_element = transport_element.get_elements("configuration")[0]
+    configurations=[]
+
+    configuration_element.elements.each do |config_name|
+      config=TransportConfiguration.new
+      config.attribute_name = config_name.name
+      config.attribute_value=config_name.text
+      configurations<<config
+    end
+
+    transport.configurations=configurations
+    transport
+  end
+
+  def get_property(element,property_name)
+   element.get_elements(property_name)[0].text
+  end
+
+
+
 end
--- web-ui/src/main/rubyapp/test/unit/helpers/configuration_helper_test.rb
+++ web-ui/src/main/rubyapp/test/unit/helpers/configuration_helper_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require "rexml/document"
 
 class ConfigurationHelperTest < ActionView::TestCase
   
@@ -27,5 +28,119 @@ class ConfigurationHelperTest < ActionView::TestCase
     assert_equal("FTP", first_conversation.get_elements("outbound_transport")[0].get_elements("type")[0].text)
     assert_equal("XML", first_conversation.get_elements("inbound_format")[0].get_elements("type")[0].text)
     assert_equal("CSV", first_conversation.get_elements("outbound_format")[0].get_elements("type")[0].text)
+  end
+
+
+
+  test "import" do
+
+    xml = "<?xml version='1.0'?>
+<systems>
+    <system>
+        <name>New System</name>
+        <description>mydescription</description>
+        <script_language>javascript</script_language>
+        <conversation>
+            <name>New Conversation</name>
+            <description>conversationdescription</description>
+            <default_response>asdfasdf</default_response>
+            <enabled>true</enabled>
+            <inbound_transport>
+                <type>File</type>
+                <configuration>
+                    <directoryName>input</directoryName>
+                    <fileName>test.xml</fileName>
+                    <pollingInterval>600</pollingInterval>
+                </configuration>
+                <type>File</type>
+            </inbound_transport>
+            <outbound_transport>
+                <type>File</type>
+                <configuration>
+                    <directoryName>output</directoryName>
+                    <fileName>output.xml</fileName>
+                    <pollingInterval>500</pollingInterval>
+                </configuration>
+            </outbound_transport>
+            <inbound_format>
+                <type>XML</type>
+                <configuration>
+                   <testPropertyName>testPropertyValue</testPropertyName>
+                </configuration>
+            </inbound_format>
+            <outbound_format>
+                <type>XML</type>
+                <configuration/>
+            </outbound_format>
+            <scenario>
+                <name>1234</name>
+                <label/>
+                <criteria_script>true</criteria_script>
+                <execution_script>var xxx ={xxx:1234}
+                    xxx
+                </execution_script>
+                <enabled>true</enabled>
+            </scenario>
+        </conversation>
+    </system>
+</systems>"
+
+    doc = REXML::Document.new(xml)
+    import_xml(doc)
+
+    system = System.find_by_name("New System")
+    assert_not_nil(system)
+    assert_equal("mydescription", system.description)
+    assert_equal("javascript", system.script_language)
+
+    conversation = Conversation.find_by_system_id(system.id)
+    assert_equal("conversationdescription", conversation.description)
+    assert_equal("asdfasdf", conversation.default_response)
+    assert_equal("File", conversation.in_transport.transport_type.name)
+    configurations = conversation.in_transport.configurations
+
+    configurations.each do|configuration|
+      case configuration.attribute_name
+        when "directoryName"
+          assert_equal "input", configuration.attribute_value
+        when "fileName"
+          assert_equal "test.xml", configuration.attribute_value
+        when "pollingInterval"
+          assert_equal "600", configuration.attribute_value
+        else
+      end
+    end
+
+    assert_equal("File", conversation.out_transport.transport_type.name)
+    configurations = conversation.out_transport.configurations
+    configurations.each do|configuration|
+      case configuration.attribute_name
+        when "directoryName"
+          assert_equal "output", configuration.attribute_value
+        when "fileName"
+          assert_equal "output.xml", configuration.attribute_value
+        when "pollingInterval"
+          assert_equal "500", configuration.attribute_value
+        else
+          fail()
+      end
+    end
+
+    assert_equal("XML", conversation.in_format.format_type.name)
+    configurations = conversation.in_format.configurations
+    configurations.each do|configuration|
+      case configuration.attribute_name
+        when "testPropertyName"
+          assert_equal "testPropertyValue", configuration.attribute_value
+        else
+          fail()
+      end
+    end
+
+    assert_equal("XML", conversation.out_format.format_type.name)
+    configurations = conversation.out_format.configurations
+    configurations.each do|configuration|
+      fail()
+    end
   end
 end
