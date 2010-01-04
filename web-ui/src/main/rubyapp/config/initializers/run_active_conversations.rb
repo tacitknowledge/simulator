fd63c312d4e6dcbcf7a2d37de04f146afcfabf5b
dvalencia <dvalencia@1caace6a-b83d-0410-a556-d4fd5157a890>
2010-01-04 23:20:24 +0000

    Fixed bug for first time rake db:migrate is run. Conversations table has not been created yet.

--- web-ui/src/main/rubyapp/config/initializers/run_active_conversations.rb
+++ web-ui/src/main/rubyapp/config/initializers/run_active_conversations.rb
@@ -1,8 +1,12 @@
 if (!ENV["RAILS_ENV"].eql?("test"))
-  enabled_conversations = Conversation.find_all_by_enabled(true)
-  puts 'Activating all enabled conversations'
-  enabled_conversations.each do |conversation|
-    puts conversation.name
-    SimulatorConnector.instance.activate(conversation);
+  begin
+    enabled_conversations = Conversation.find_all_by_enabled(true)
+    puts 'Activating all enabled conversations'
+    enabled_conversations.each do |conversation|
+      puts conversation.name
+      SimulatorConnector.instance.activate(conversation);
+    end
+  rescue => e
+    puts e.message
   end
 end
