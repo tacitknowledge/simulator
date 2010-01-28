859d840590c9680460bc91027252dfe8bf4792d8
dvalencia <dvalencia@1caace6a-b83d-0410-a556-d4fd5157a890>
2010-01-28 00:20:09 +0000

    Handling java exceptions from jruby. conversations_controller

--- web-ui/src/main/rubyapp/app/controllers/conversations_controller.rb
+++ web-ui/src/main/rubyapp/app/controllers/conversations_controller.rb
@@ -5,9 +5,14 @@ class ConversationsController < ApplicationController
     if (params[:format]=='json')
       @conversations = Conversation.find_all_by_system_id(params[:system_id])
 
-      connector = SimulatorConnector.instance
-      @conversations.each do|conversation|                                                                    
-        conversation[:active]= connector.is_active(conversation)
+      begin
+        connector = SimulatorConnector.instance
+        @conversations.each do|conversation|
+          conversation[:active]= connector.is_active(conversation)
+        end
+      rescue java.lang.Exception => ex
+        logger.info(ex)
+        render :json => { :message => "We had a problem retrieving the conversations"} and return
       end
       render :json => { :success=>true, :data => @conversations }
     else
@@ -18,7 +23,7 @@ class ConversationsController < ApplicationController
 
   def show
     # No need to start loading data from DB if it's not a JSON request
-    if (params[:format]=='json')
+    if(params[:format] == 'json')
       @conversation = Conversation.find(params[:id])
       @conversation[:inbound_transport_type_id] = @conversation.in_transport.transport_type_id
       @conversation[:outbound_transport_type_id] = @conversation.out_transport.transport_type_id
@@ -34,41 +39,46 @@ class ConversationsController < ApplicationController
       # Include the configurations in the JSON response
       configurations = {}
 
-      # Transports
-      transport_in = {}
-      transport_in[:parameters] = SimulatorConnector.instance.get_transport_parameters(in_tt_name)
-      transport_in[:config_values] =
-              TransportConfiguration.find_all_by_transport_id(@conversation.inbound_transport_id)
-      configurations[:transport_in] = transport_in
+      begin
+        # Transports
+        transport_in = {}
+        transport_in[:parameters] = SimulatorConnector.instance.get_transport_parameters(in_tt_name)
+        transport_in[:config_values] =
+                TransportConfiguration.find_all_by_transport_id(@conversation.inbound_transport_id)
+        configurations[:transport_in] = transport_in
 
-      transport_out = {}
-      transport_out[:parameters] = SimulatorConnector.instance.get_transport_parameters(out_tt_name)
-      transport_out[:config_values] =
-              TransportConfiguration.find_all_by_transport_id(@conversation.outbound_transport_id)
-      configurations[:transport_out] = transport_out
+        transport_out = {}
+        transport_out[:parameters] = SimulatorConnector.instance.get_transport_parameters(out_tt_name)
+        transport_out[:config_values] =
+                TransportConfiguration.find_all_by_transport_id(@conversation.outbound_transport_id)
+        configurations[:transport_out] = transport_out
 
-      # Formats
-      format_in = {}
-      format_in[:parameters] = SimulatorConnector.instance.get_format_parameters(in_frmt_name)
-      format_in[:config_values] =
-              FormatConfiguration.find_all_by_format_id(@conversation.inbound_format_id)
-      configurations[:format_in] = format_in
+        # Formats
+        format_in = {}
+        format_in[:parameters] = SimulatorConnector.instance.get_format_parameters(in_frmt_name)
+        format_in[:config_values] =
+                FormatConfiguration.find_all_by_format_id(@conversation.inbound_format_id)
+        configurations[:format_in] = format_in
 
-      # Formats
-      format_out = {}
-      format_out[:parameters] = SimulatorConnector.instance.get_format_parameters(out_frmt_name)
-      format_out[:config_values] =
-              FormatConfiguration.find_all_by_format_id(@conversation.outbound_format_id)
-      configurations[:format_out] = format_out
+        # Formats
+        format_out = {}
+        format_out[:parameters] = SimulatorConnector.instance.get_format_parameters(out_frmt_name)
+        format_out[:config_values] =
+                FormatConfiguration.find_all_by_format_id(@conversation.outbound_format_id)
+        configurations[:format_out] = format_out
 
 
-      msg = {}
-      msg[:success] = true
-      msg[:data] = @conversation
-      msg[:configurations] = configurations
+        msg = {}
+        msg[:success] = true
+        msg[:data] = @conversation
+        msg[:configurations] = configurations
 
-      logger.debug(msg.to_json)
-      
+        logger.debug(msg.to_json)
+      rescue java.lang.Exception => ex
+        logger.info(ex)
+        render :json => { :message => "We had a problem retrieving the conversation"} and return
+      end
+
       render :json => msg.to_json
     end
   end
@@ -77,9 +87,14 @@ class ConversationsController < ApplicationController
     conversation = populate_conversation(params)
 
     if conversation.save
-      SimulatorConnector.instance.create_or_update_conversation(conversation)
-      flash[:notice] = "Successfully created new Conversation '#{conversation.name}' with id #{conversation.id}"
-      render :json => { :success => true, :data => conversation }
+      begin
+        SimulatorConnector.instance.create_or_update_conversation(conversation)
+        flash[:notice] = "Successfully created new Conversation '#{conversation.name}' with id #{conversation.id}"
+        render :json => { :success => true, :data => conversation }
+      rescue java.lang.Exception => ex
+        logger.info(ex)
+        render :json => { :message => "We had a problem creating the conversation"} and return
+      end
     else
       logger.debug("Unable to save Conversation. List of Errors follow up:")
       logger.debug("     #{conversation.errors.full_messages  }")
@@ -90,46 +105,50 @@ class ConversationsController < ApplicationController
 
   def update
 #   remove conversation from the system if it's currently running
-    conversation = Conversation.find params[:id]
-    was_active=false
-    if(SimulatorConnector.instance.is_active(conversation))
-      puts 'removing conversation'
-      was_active=true
-      SimulatorConnector.instance.deactivate(conversation)
-    else
-      puts 'not deactivating'
-    end
-    conversation = update_conversation(params, conversation)
-#   restart conversation using new configuration
-    if conversation.save
-      jconvers = SimulatorConnector.instance.create_or_update_conversation(conversation)
-      conversation.scenarios.each do |scenario|
-            system = System.find(scenario.conversation.system_id)
-            script_language = system.script_language
-            jconvers.addOrUpdateScenario( scenario.id, script_language, scenario.criteria_script, scenario.execution_script)
-        end
-      if(was_active)
-        SimulatorConnector.instance.activate(conversation)
-        puts 'activating'
-      else
-        puts 'not activating'
+    begin
+      conversation = Conversation.find params[:id]
+      was_active=false
+      if(SimulatorConnector.instance.is_active(conversation))
+        was_active=true
+        SimulatorConnector.instance.deactivate(conversation)
       end
-      msg = "Successfully updated conversation '#{conversation.name}' with id #{conversation.id}"
-      render :json => { :success => true, :message => msg, :data => conversation }
-    else
-      render :json => { :message => "Failed to update Conversation"}
+      conversation = update_conversation(params, conversation)
+  #   restart conversation using new configuration
+      if conversation.save
+        jconvers = SimulatorConnector.instance.create_or_update_conversation(conversation)
+        conversation.scenarios.each do |scenario|
+              system = System.find(scenario.conversation.system_id)
+              script_language = system.script_language
+              jconvers.addOrUpdateScenario( scenario.id, script_language, scenario.criteria_script, scenario.execution_script)
+          end
+        if(was_active)
+          SimulatorConnector.instance.activate(conversation)
+        end
+        msg = "Successfully updated conversation '#{conversation.name}' with id #{conversation.id}"
+        render :json => { :success => true, :message => msg, :data => conversation }
+      else
+        render :json => { :message => "Failed to update Conversation"}
+      end
+    rescue java.lang.Exception => ex
+      logger.info(ex)
+      render :json => { :message => "We had a problem updating conversation #{conversation.name}"}
     end
   end
 
   def destroy
-    @conversation = Conversation.find(params[:id])
-    conv_name = @conversation.name
-    SimulatorConnector.instance.delete_conversation(@conversation)
-    if @conversation.destroy
-      msg = "Successfully deleted Conversation '#{conv_name}' with id #{params[:id]}"
-      render :json => { :success => true, :message => msg}
-    else
-      render :json => { :message => "Failed to destroy Conversation" }
+    begin
+      @conversation = Conversation.find(params[:id])
+      conv_name = @conversation.name
+      SimulatorConnector.instance.delete_conversation(@conversation)
+      if @conversation.destroy
+        msg = "Successfully deleted Conversation '#{conv_name}' with id #{params[:id]}"
+        render :json => { :success => true, :message => msg}
+      else
+        render :json => { :message => "Failed to destroy Conversation" }
+      end
+    rescue java.lang.Exception => ex
+      logger.info(ex)
+      render :json => { :message => "We had a problem destroying conversation #{@conversation.name}"}
     end
   end
 
@@ -195,8 +214,13 @@ class ConversationsController < ApplicationController
   def transport_parameters
     data = [] 
     if !params[:type].nil? && !params[:type].empty?
-      transport_class_name = TransportType.get_transport_class_name_by_name(params[:type])
-      data = SimulatorConnector.instance.get_transport_parameters(transport_class_name)
+      begin
+        transport_class_name = TransportType.get_transport_class_name_by_name(params[:type])
+        data = SimulatorConnector.instance.get_transport_parameters(transport_class_name)
+      rescue java.lang.Exception => ex
+        logger.info(ex)
+        render :json => {:message => "An error ocurred retrieving transport parameters."} and return
+      end
     end
     render :json => {:data => data}
   end
@@ -204,8 +228,13 @@ class ConversationsController < ApplicationController
   def format_parameters
     data = []
     if !params[:format].nil? && !params[:format].empty?
-      format_class_name = FormatType.get_format_class_name_by_name(params[:format])
-      data = SimulatorConnector.instance.get_format_parameters(format_class_name)
+      begin
+        format_class_name = FormatType.get_format_class_name_by_name(params[:format])
+        data = SimulatorConnector.instance.get_format_parameters(format_class_name)
+      rescue java.lang.Exception => ex
+        logger.info(ex)
+        render :json => {:message => "An error ocurred retrieving format parameters."} and return
+      end
     end
     render :json => {:data => data}
   end
@@ -215,7 +244,12 @@ class ConversationsController < ApplicationController
     conversation.enabled=!conversation.enabled
 #    if new value is disabled then deactivate
     if (!conversation.enabled)
-      SimulatorConnector.instance.deactivate(conversation)
+      begin
+        SimulatorConnector.instance.deactivate(conversation)
+      rescue java.lang.Exception => ex
+        logger.info(ex)
+        render :json => {:message => "An error ocurred enabling conversation #{conversation.name}."} and return
+      end
     end
     conversation.save
     render :json => { :success=>true, :data => conversation}
@@ -225,15 +259,20 @@ class ConversationsController < ApplicationController
   #is inactive => deactivates
   def activate
     conversation = Conversation.find(params[:id])
-    if (conversation.enabled)
-      is_active = SimulatorConnector.instance.is_active(conversation)
-      if(is_active)
-        SimulatorConnector.instance.deactivate(conversation)
-      else
-        SimulatorConnector.instance.activate(conversation)
+    begin
+      if (conversation.enabled)
+        is_active = SimulatorConnector.instance.is_active(conversation)
+        if(is_active)
+          SimulatorConnector.instance.deactivate(conversation)
+        else
+          SimulatorConnector.instance.activate(conversation)
+        end
       end
+      is_active = SimulatorConnector.instance.is_active(conversation)
+    rescue java.lang.Exception => ex
+        logger.info(ex)
+        render :json => {:message => "An error ocurred activating conversation #{conversation.name}."} and return
     end
-    is_active = SimulatorConnector.instance.is_active(conversation)
     render :json => { :success=>true, :data => conversation, :is_active => is_active}
   end
 end
