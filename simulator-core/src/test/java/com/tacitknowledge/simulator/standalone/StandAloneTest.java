f278e966a0195be3fdd3d708630a7246a05bc607
ociobanu <ociobanu@1caace6a-b83d-0410-a556-d4fd5157a890>
2011-03-09 15:42:22 +0000

    code refactoring

--- simulator-core/src/test/java/com/tacitknowledge/simulator/standalone/StandAloneTest.java
+++ simulator-core/src/test/java/com/tacitknowledge/simulator/standalone/StandAloneTest.java
@@ -5,44 +5,48 @@ import java.io.IOException;
 import junit.framework.Assert;
 
 import org.junit.Test;
-/**
- * Test that a close command is detected on the close port and this is detected by ConversationsScheduledLoader  
+
+/** Test that a close command is detected on the close port
+ *  and this is detected by ConversationsScheduledLoader.
  * @author Oleg Ciobanu ociobanu@tacitknowledge.com
  */
 public class StandAloneTest {
-	@Test
-	public void test()
-	{
-		try
-		{
-			// initialize the "conversations scheduled loader"  
-			ConversationsScheduledLoader loader = new ConversationsScheduledLoader();
-			// initialize a close command waiter.
-			CloseCommandWaiter closeWaiter = new CloseCommandWaiter();
-			
-			closeWaiter.setConversationsScheduledLoader(loader) ;
-			
-			// start "file loader" thread and "close command waiter" thread
-			loader.start();
-			closeWaiter.start();
-			
-			// send close command
-			StandAloneStopper stoper = new StandAloneStopper();
-			stoper.sendCloseCommand();
-			try
-			{
-				// wait the close command propagation through socket. 
-				Thread.currentThread().sleep(500);
-			}
-			catch (InterruptedException e)
-			{
-			}
-			// assert the close command was detected.
-			Assert.assertEquals(Boolean.TRUE, loader.getToStop());
-		}
-		catch (IOException e)
-		{
-			Assert.fail(e.getMessage());
-		}
-	}
+    /**
+     * Number of milliseconds to wait for close command propagation.
+     */
+    private static final int MILLISECONDS_TO_WAIT_500 = 500;
+
+    /**
+     * Test close command propagation.
+     */
+    @Test
+    public void test() {
+        try {
+            // initialize the "conversations scheduled loader"
+            ConversationsScheduledLoader loader =
+                new ConversationsScheduledLoader();
+            // initialize a close command waiter.
+            CloseCommandWaiter closeWaiter = new CloseCommandWaiter();
+
+            closeWaiter.setConversationsScheduledLoader(loader);
+
+            // start "file loader" thread and "close command waiter" thread
+            loader.start();
+            closeWaiter.start();
+
+            // send close command
+            StandAloneStopper stoper = new StandAloneStopper();
+            stoper.sendCloseCommand();
+            try {
+                // wait the close command propagation through socket.
+                Thread.currentThread().sleep(MILLISECONDS_TO_WAIT_500);
+            } catch (InterruptedException e) {
+                Assert.fail(e.getMessage());
+            }
+            // assert the close command was detected.
+            Assert.assertEquals(Boolean.TRUE, loader.getToStop());
+        } catch (IOException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
 }
