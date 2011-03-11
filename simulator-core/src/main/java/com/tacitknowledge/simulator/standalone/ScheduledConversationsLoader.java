81c65e7d2bf9c88b496825c05022777a860220f9
mtricolici <mtricolici@1caace6a-b83d-0410-a556-d4fd5157a890>
2011-03-11 12:26:49 +0000

    fixing some warnings

--- simulator-core/src/main/java/com/tacitknowledge/simulator/standalone/ScheduledConversationsLoader.java
+++ simulator-core/src/main/java/com/tacitknowledge/simulator/standalone/ScheduledConversationsLoader.java
@@ -1,16 +1,11 @@
 package com.tacitknowledge.simulator.standalone;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
-import com.tacitknowledge.simulator.BaseConfigurable;
 import com.tacitknowledge.simulator.Conversation;
 import com.tacitknowledge.simulator.RouteManager;
 import com.tacitknowledge.simulator.camel.RouteManagerImpl;
--- simulator-core/src/main/java/com/tacitknowledge/simulator/standalone/StandAloneStopper.java
+++ simulator-core/src/main/java/com/tacitknowledge/simulator/standalone/StandAloneStopper.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
