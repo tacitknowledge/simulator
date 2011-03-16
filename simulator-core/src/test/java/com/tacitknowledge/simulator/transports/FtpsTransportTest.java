d11cece6d30d82c1bf46a0f1bcc48a69f80e7432
ociobanu <ociobanu@1caace6a-b83d-0410-a556-d4fd5157a890>
2011-03-16 16:02:57 +0000

    Unit tests for ftps active mode validation.

--- simulator-core/src/test/java/com/tacitknowledge/simulator/transports/FtpsTransportTest.java
+++ simulator-core/src/test/java/com/tacitknowledge/simulator/transports/FtpsTransportTest.java
@@ -13,7 +13,18 @@ import com.tacitknowledge.simulator.Transport;
 
 public class FtpsTransportTest
 {
+    private static final String FTPS_URI_PREFIX = "ftps://127.0.0.1";
+
+    private static final String LOCALHOST_IP = "127.0.0.1";
+
+    private static final String FAILED_BAD_VALIDATION_MESSAGE = "Validation with incomplete parameters should throw an exception";
+
+    private static final String PASSWORD = "password";
+
+    private static final String KEY_FILE_NAME = "filename.jks";
+
     private Transport transport;
+
     private Map<String, String> params;
 
     @Before
@@ -22,12 +33,12 @@ public class FtpsTransportTest
         transport = new FtpsTransport();
         params = new HashMap<String, String>();
     }
-    
+
     @Test
     public void testGetFftspUri()
     {
         // --- Try to get this URI: sftp://127.0.0.1
-        params.put(FtpTransport.PARAM_HOST, "127.0.0.1");
+        params.put(FtpTransport.PARAM_HOST, LOCALHOST_IP);
 
         transport.setParameters(params);
 
@@ -35,12 +46,56 @@ public class FtpsTransportTest
         {
             String uri = transport.toUriString();
 
-            assertTrue("Returned uri isn't as expected: " + uri,
-                uri.indexOf("ftps://127.0.0.1") > -1);
+            assertTrue("Returned uri isn't as expected: " + uri, uri.indexOf(FTPS_URI_PREFIX) > -1);
         }
         catch (Exception e)
         {
             fail("Shouldn't be getting an exception here: " + e.getMessage());
         }
+    }
+
+    @Test
+    public void testGoodValidation()
+    {
+        // --- Try to get this URI: sftp://127.0.0.1
+        params.put(FtpTransport.PARAM_HOST, LOCALHOST_IP);
+        params.put(FtpsTransport.KEY_STORE_FILE, KEY_FILE_NAME);
+        params.put(FtpsTransport.KEY_STORE_PASSWORD, PASSWORD);
+        params.put(FtpsTransport.KEY_STORE_KEY_PASSWORD, PASSWORD);
+
+        transport.setParameters(params);
+
+        try
+        {
+            String uri = transport.toUriString();
+
+            assertTrue(uri.indexOf(FtpsTransport.FTP_CLIENT_KEY_STORE_FILE) > -1);
+        }
+        catch (Exception e)
+        {
+            fail("Shouldn't be getting an exception here: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testBadValidation()
+    {
+        // --- Try to get this URI: sftp://127.0.0.1
+        params.put(FtpTransport.PARAM_HOST, LOCALHOST_IP);
+        params.put(FtpsTransport.KEY_STORE_FILE, KEY_FILE_NAME);
+        params.put(FtpsTransport.KEY_STORE_PASSWORD, PASSWORD);
+
+        transport.setParameters(params);
+
+        try
+        {
+            String uri = transport.toUriString();
+            fail(FAILED_BAD_VALIDATION_MESSAGE);
+        }
+        catch (Exception e)
+        {
+            assertTrue(e.getMessage().startsWith(FtpsTransport.REQUIRED_PARAMETERS_MESSAGE));
+        }
+
     }
 }
